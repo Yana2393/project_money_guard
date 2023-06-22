74e7bafed63bef3903cff89dc426aa6e2679c8fb
Tensys57 <tensys57@gmail.com>
2023-06-22 18:56:45 +0300

    part 1

--- src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
 const ModalAddTransaction = () => {
-  return <div>ModalAddTransaction</div>;
+  const status = {
+    Income: 'income',
+    Expence: 'expence',
+  };
+
+  const handleChange = () => {};
+
+  return (
+    <div>
+      <section>
+        <h2>Add transaction</h2>
+        <label>
+          Income
+          <input
+            type="radio"
+            checked={status === status.Income}
+            name="income"
+            value={status.Income}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Expence
+          <input
+            type="radio"
+            checked={status === status.Expence}
+            name="expence"
+            value={status.Expence}
+            onChange={handleChange}
+          />
+        </label>
+      </section>
+    </div>
+  );
 };
 
 export default ModalAddTransaction;
