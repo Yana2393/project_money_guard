e7014f166cdfbec6954771c778718492f1d3c131
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-06-22 18:19:16 +0300

    commit api

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import css from './Header.module.css';
 
 const Header = () => {
-  return <div className={css.header}>Header</div>;
+  return <div className={css.header}>Header Headre </div>;
 };
 
 export default Header;
