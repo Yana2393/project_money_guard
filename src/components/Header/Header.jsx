ea52f100ce7125d004f2492eb43a04ee84b1da95
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-06-23 17:06:42 +0300

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,9 @@
 import css from './Header.module.css';
-import ButtonAddTransactions from 'components/ButtonAddTransactions/ButtonAddTransactions';
 
 const Header = () => {
   return (
     <div className={css.header}>
       Header Headre
-      <ButtonAddTransactions />
       <button> ВИЙТИ</button>
     </div>
   );
