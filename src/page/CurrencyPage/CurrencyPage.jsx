bbe574dc4cd662839dbe926055df45f500217d61
Hrustch <nik.stalker47@gmail.com>
2023-06-23 19:00:36 +0300

    Set and done

--- src/page/CurrencyPage/CurrencyPage.jsx
+++ src/page/CurrencyPage/CurrencyPage.jsx
@@ -1,7 +1,25 @@
 import css from './CurrencyPage.module.css';
 
 const CurrencyPage = () => {
-  return <div className={css.CurrencyPage}>CurrencyPage</div>;
+  return <div className={css.CurrencyPage}>
+  <table className={css.Currency}>
+    <tr>
+      <th>Curency</th>
+      <th>Purchase</th>
+      <th>Sale</th>
+    </tr>
+    <tr>
+      <td>USD</td>
+      <td>usdPurch</td>
+      <td>usdSale</td>
+    </tr>
+    <tr>
+      <td>EUR</td>
+      <td>eurPurch</td>
+      <td>eurSale</td>
+    </tr>
+  </table>
+</div>;
 };
 
 export default CurrencyPage;
