7162a3350d25ff9b210b5ce713f80c70ee6ea87b
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-06-24 15:11:36 +0300

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
 import css from './Header.module.css';
+import { FaBeer } from 'react-icons/fa';
+
+import { IoMdExit } from 'react-icons/io';
 
 const Header = () => {
   return (
     <div className={css.header}>
       <div className="container">
-        Header Headre
-        <button> ВИЙТИ</button>
+        <div className={css.headerInfo}>
+          <img src="" alt="logo" />
+          <button>
+            <IoMdExit />
+          </button>
+        </div>
       </div>
     </div>
   );
