200e421edec83429d0f33468e5d0169862e575bf
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-06-24 18:52:20 +0300

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,21 +2,33 @@ import { userLogOut } from 'redux/Auth/authOperations';
 import css from './Header.module.css';
 
 import { IoMdExit } from 'react-icons/io';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectCurrentUser } from 'redux/Auth/authSelector';
+import { selectViewPort } from 'redux/Viewport/viewportSelectors';
+import logo from '../../images/logo.png';
 
 const Header = () => {
   const dispatch = useDispatch();
+  const currentUser = useSelector(selectCurrentUser);
+  const viewport = useSelector(selectViewPort);
   const handleLogOut = () => {
     dispatch(userLogOut());
   };
+
   return (
     <div className={css.header}>
       <div className="container">
         <div className={css.headerInfo}>
-          <img src="" alt="logo" />
-          <button onClick={handleLogOut}>
-            <IoMdExit />
-          </button>
+          <img src={logo} alt="logo" width="86px" height="36px" />
+          <div className={css.headerRigth}>
+            <span className={css.userText}>
+              {currentUser?.username && currentUser.username}
+            </span>
+            <button className={css.headerBtn} onClick={handleLogOut}>
+              <IoMdExit className={css.iconBtn} />
+              {!viewport.mobile && <span className={css.userText}>Exit</span>}
+            </button>
+          </div>
         </div>
       </div>
     </div>
