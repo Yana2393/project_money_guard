3fa7bd4366b8eab425a378dabe19b57a6215d68d
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-06-24 19:35:14 +0300

    commit

--- src/page/CurrencyPage/CurrencyPage.jsx
+++ src/page/CurrencyPage/CurrencyPage.jsx
@@ -1,7 +1,12 @@
 import Currency from 'components/Currency/Currency';
+import css from './CurrencyPage.module.css';
 
 const CurrencyPage = () => {
-  return <Currency />;
+  return (
+    <div className={css.CurrencyPage}>
+      <Currency />
+    </div>
+  );
 };
 
 export default CurrencyPage;
