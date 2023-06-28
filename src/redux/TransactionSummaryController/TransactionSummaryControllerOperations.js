081b667b8106b02b8980cd68f1b6afc703b00c0d
olegvoskovets <105343324+olegvoskovets@users.noreply.github.com>
2023-06-28 11:56:57 +0300

    commit

--- src/redux/TransactionSummaryController/TransactionSummaryControllerOperations.js
+++ src/redux/TransactionSummaryController/TransactionSummaryControllerOperations.js
@@ -17,23 +17,3 @@ export const getSummary = createAsyncThunk(
     }
   }
 );
-
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-
-// import { Api } from 'redux/api/Api';
-// //import { token } from 'redux/api/Api';
-
-// export const getSummary = createAsyncThunk(
-//   'transactions/getSummary',
-//   async (period, thunk_Api) => {
-//     try {
-//       const { data } = await Api.get(
-//         `transactions-summary?month=${period.month}&year=${period.year}`
-//       );
-
-//       return data;
-//     } catch (error) {
-//       return thunk_Api.rejectWithValue(error.message);
-//     }
-//   }
-// );
